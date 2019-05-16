3ae9e55fa543b872605eed5347dbf0fbc520341d
Krisli Dimo <krislidimo@gmail.com>
2019-05-16 17:15:57 -0500

    added routes, links, and PrivatRoute

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import PrivateRoute from './PrivateRoute';
+import Login from './components/Login.js';
+import Protected from './components/Protected.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/protected">Protected Page</Link>
+        </div>  
+        <Route path='/login' component={Login}/>
+        <PrivateRoute exact path='/protected' component={Protected}/>
+      </div>
+    </Router>
   );
 }
 
